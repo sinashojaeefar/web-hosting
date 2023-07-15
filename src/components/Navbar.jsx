ed95f743005d1ca3a14b05e9a69865f47c35c2a5
sinashojaeefar <sina.shojaeefar@gmail.com>
2023-07-15 15:56:48 +0330

    package.json update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
              {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/> }
         </div>
 
-            <div className= {!nav ? 'fixed md:hidden left-0 top-0 h-full w-[60%] border-r border-r-gray-400 bg-gray-300 ease-in-out duration-500' : 'fixed left-[-100%]' } >
+            <div className= {!nav ? 'fixed md:hidden z-10 left-0 top-0 h-full w-[60%] border-r border-r-gray-400 bg-gray-300 ease-in-out duration-500' : 'fixed left-[-100%]' } >
         <h1 className='w-full text-3xl font-bold text-[#273c75] m-4'>Web Hosting</h1>
             <ul className='pt-8'>
               <li className='p-4 border-b border-gray-400'>Home</li>
